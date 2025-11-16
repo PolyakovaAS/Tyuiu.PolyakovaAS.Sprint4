65e8a51820fd830eebe304709e8b4e25cae5f3ca
KirillBurdov <Patrikandbooke@gmail.com>
2025-11-16 13:05:49 +0300

    Add project files.

---
+++ Tyuiu.PolyakovaAS.Sprint4.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.PolyakovaAS.Sprint4.Task0.V9.Lib;
+namespace Tyuiu.PolyakovaAS.Sprint4.Task0.V9.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 4, 6, 2, 8, 4, 5, 6, 9, 8, 7 };
+            int result = ds.GetSumEvenArrEl(array);
+
+            int expected = 4 + 6 + 2 + 8 + 4 + 6 + 8; 
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Tyuiu.PolyakovaAS.Sprint4.Task0.V9/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.PolyakovaAS.Sprint4.Task0.V9.Lib;
+int[] array = { 4, 6, 2, 8, 4, 5, 6, 9, 8, 7 };
+
+Console.Write("Массив: { ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i]);
+    if (i < array.Length - 1) Console.Write(", ");
+}
+Console.WriteLine(" }");
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+DataService ds = new DataService();
+int result = ds.GetSumEvenArrEl(array);
+
+Console.WriteLine($"Сумма четных элементов массива = {result}");
+
+Console.ReadLine();
